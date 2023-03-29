df21604593e0c7ae5e3bc351fc616c32cdbba6e7
Luisetex <luisetex@gmail.com>
2023-03-29 21:43:22 +0200

    add argilla generator

--- src/argilla_client.py
+++ src/argilla_client.py
@@ -1,22 +1,27 @@
-from typing import Optional, List
+from typing import Generator, List, Optional
 
 import argilla as rg
+
 from src import logger
 
 
 def get_alpaca_es_client():
     """Get the alpaca-es database from argilla"""
     import argilla as rg
-    rg.init(api_key="team.apikey",
-            api_url="https://mserras-somos-alpaca-es.hf.space",
-            workspace="team")
+
+    rg.init(
+        api_key="team.apikey", api_url="https://mserras-somos-alpaca-es.hf.space", workspace="team"
+    )
     return rg
 
 
 def get_alpaca_en_client():
     """get the alpaca-en dataset from argilla"""
     import argilla as rg
-    rg.init(api_key="team.apikey", api_url="https://mserras-somos-alpaca-en.hf.space", workspace="team")
+
+    rg.init(
+        api_key="team.apikey", api_url="https://mserras-somos-alpaca-en.hf.space", workspace="team"
+    )
     return rg
 
 
@@ -25,9 +30,9 @@ def get_prompt_client():
     pass
 
 
-def load_dataset_from_argilla(rg_agent, dataset_name: str,
-                              limit: int = -1,
-                              query: Optional[str] = None) -> List[rg.TextClassificationRecord]:
+def load_dataset_from_argilla(
+    rg_agent, dataset_name: str, limit: int = -1, query: Optional[str] = None
+) -> List[rg.TextClassificationRecord]:
     """
     Load a dataset from Argilla. If limit is -1, it will load all the dataset. The search can be filtered using
     an ElasticSearch query
@@ -52,14 +57,50 @@ def load_dataset_from_argilla(rg_agent, dataset_name: str,
         while initial_batch:
             records += initial_batch
             logger.debug(f"[RB] Total loaded {len(records)}")
-            initial_batch = rg_agent.load(dataset_name, limit=limit, query=query, id_from=initial_batch[-1].id)
+            initial_batch = rg_agent.load(
+                dataset_name, limit=limit, query=query, id_from=initial_batch[-1].id
+            )
             logger.debug(f"[RB] Loaded a batch of {len(records)} from {dataset_name}.")
     else:
         records = rg_agent.load(dataset_name, limit=limit, query=query)
-    logger.success(f"[RB]A total of {len(records)} samples loaded from {dataset_name}! Casting them to Sample.")
+    logger.success(
+        f"[RB]A total of {len(records)} samples loaded from {dataset_name}! Casting them to Sample."
+    )
     return list(records)
 
 
-if __name__ == '__main__':
+def argilla_dataset_generator(
+    rg_agent, dataset_name: str, query: str | None
+) -> Generator[rg.TextClassificationRecord, None, None]:
+    """
+    Generator that yields a dataset from Argilla. The search can be filtered using an ElasticSearch query
+
+    Parameters
+    ----------
+    rg_agent: argilla agent to use
+    dataset_name: str - name of the dataset to load
+    query: str - ElasticSearch query to filter the results
+
+    Yields
+    -------
+    TextClassificationRecord
+
+    """
+    limit = 1
+    initial_batch = rg_agent.load(dataset_name, limit=limit, query=query)
+    logger.debug(f"[RB] Loaded the initial batch of {limit} from {dataset_name}.")
+    while initial_batch:
+        yield initial_batch[0]
+        initial_batch = rg_agent.load(
+            dataset_name, limit=limit, query=query, id_from=initial_batch[-1].id
+        )
+        logger.debug(f"[RB] Loaded a batch of {len(initial_batch)} from {dataset_name}.")
+    logger.success(
+        f"[RB]A total of {len(initial_batch)} samples loaded from {dataset_name}! Casting them to"
+        " Sample."
+    )
+
+
+if __name__ == "__main__":
     client = get_alpaca_es_client()
     print(client.load("somos-alpaca-es", limit=1)[0])
